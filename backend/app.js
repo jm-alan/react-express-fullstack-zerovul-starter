ed84c4b1a64f0edc78450972eb5b3ffb2faee75a
Joshua <j.maxwell.alan@gmail.com>
2021-07-11 01:07:41 -0400

    eslintconfig

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,10 @@
 import appBuilder from './appBuilder';
 import { ports } from './config';
 
-// eslint-disable-next-line no-extend-native
 String.prototype.upperCaseFirst = function () {
   return `${this[0].toUpperCase()}${this.slice(1)}`;
 };
 
-// eslint-disable-next-line no-extend-native
 String.prototype.truncateUntil = function (pattern) {
   let out = this.toString();
   while (!out.match(pattern)) out = out.slice(0, out.length - 1);
