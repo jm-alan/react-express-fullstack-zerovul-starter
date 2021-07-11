669fec46061a68a1376a7dd215353a3ee62cdd83
Joshua <j.maxwell.alan@gmail.com>
2021-07-11 01:03:36 -0400

    pare router back to basics

--- backend/router/api/index.js
+++ backend/router/api/index.js
@@ -1,12 +1,10 @@
 import sessionRouter from './session';
 import userRouter from './users';
-import accountsRouter from './accounts';
 import { Router } from 'express';
 const router = Router();
 
 router.use('/session', sessionRouter);
 router.use('/users', userRouter);
-router.use('/accounts', accountsRouter);
 
 router.get('/csrf/restore/', (req, res) => {
   const token = req.csrfToken();
